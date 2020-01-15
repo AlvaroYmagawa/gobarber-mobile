15489d6ddd6966e7a982440f32cdaf7dcc6bfdee
Kenzo Ymagawa <alvaroymagawa@gmail.com>
2020-01-14 22:42:41 -0300

    feat: add Redux configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReacotronConfig';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
 export default function src() {
-  return <>
-  <StatusBar barStyle="light-conttent" backgroundColor="#7159c1" />
-  <Routes />
-  </>
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-conttent" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+  </Provider>
+  );
 }
